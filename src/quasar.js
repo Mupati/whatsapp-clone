7eb58d6bb6113a8670a363a9e0b10a85223fcafe
Mupati <scientificgh@gmail.com>
2021-01-01 13:51:54 +0000

    Initial commit

---
+++ src/quasar.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+
+import "./styles/quasar.scss";
+import "quasar/dist/quasar.ie.polyfills";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/fontawesome-v5/fontawesome-v5.css";
+import { Quasar } from "quasar";
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {}
+});
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from "@vue/test-utils";
+import { render } from "@testing-library/vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const { getByText } = render(HelloWorld, {
+      props: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+    getByText(msg);
   });
 });
