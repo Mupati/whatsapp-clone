9d99d34c461be2cd6da07e4f5151a23d0bdbd1bf
Mupati <scientificgh@gmail.com>
2021-01-04 14:57:12 +0000

    remove unneeded test

--- tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/vue";
-import NoSelectedMessage from "@/components/NoSelectedMessage.vue";
-
-describe("NoSelectedMessage.vue", () => {
-  it("renders no message feedback", () => {
-    render(NoSelectedMessage);
-    screen.getByText(/Keep/i);
-  });
-});
